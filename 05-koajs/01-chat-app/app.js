9a2ba5abfd35f93b112b05d4ff0e131ed7b48f52
Anton Fedorov <anton.pzkt@gmail.com>
2021-10-14 03:08:43 +0300

    added solution for task 1 module 5

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,44 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = {};
+
+const sendMessage = (message) => {
+	if (!message) return;
+
+	Object.values(subscribers).map(subscriber => {
+		subscriber(message);
+	});
+
+	subscribers = {};
+};
+
 router.get('/subscribe', async (ctx, next) => {
+	try {
+		const id = ctx.originalUrl.split('=')[1];
+
+		const message = await new Promise(resolve => {
+			subscribers[id] = resolve;
+		});
+
+		ctx.body = message;
+	} catch(err) {
+		console.log(err);
+
+		delete subscribers[id];
+	}
 });
 
 router.post('/publish', async (ctx, next) => {
+	try {
+		const message = ctx.request.body.message;
+
+		sendMessage(message);
+
+		ctx.body = 'success';
+	} catch(err) {
+		console.log(err);
+	}
 });
 
 app.use(router.routes());
